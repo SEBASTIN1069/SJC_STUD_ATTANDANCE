211d6f295433026405262237d565963b928ebf3c
SEBASTIN1069 <alwin807209@gmail.com>
2026-05-07 13:01:07 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+self.addEventListener("install", e => {
+
+    e.waitUntil(
+
+        caches.open("attendance-store").then(cache => {
+
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./style.css",
+                "./bg.jpg",
+                "./icon.jpg"
+            ]);
+
+        })
+
+    );
+
+});
+
+self.addEventListener("fetch", e => {
+
+    e.respondWith(
+
+        caches.match(e.request).then(response => {
+
+            return response || fetch(e.request);
+
+        })
+
+    );
+
+});
